368d432a8e34b6219d66f01daf2134807b9c4a68
Kostas <Ziovas>
2021-12-05 13:47:26 +0200

    Added pytest-asyncio, added home endpoint test and updated requirements

---
+++ tests/test_home_endpoint.py
@@ -0,0 +1,11 @@
+import pytest
+import aiohttp
+
+
+@pytest.mark.asyncio
+async def test_storage_status():
+    """This test assumes that the PetStore API service is running"""
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get("http://localhost:8000/") as response:
+            assert response.status == 200
