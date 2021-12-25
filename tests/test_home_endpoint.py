e8a4b7caa7d3c7f67cfb2d8226c98103f5e53a30
Kostas <Ziovas>
2021-12-25 13:16:08 +0200

    [WIP] Added time delay in home endpoint test

--- tests/test_home_endpoint.py
+++ tests/test_home_endpoint.py
@@ -1,10 +1,14 @@
 import pytest
 import aiohttp
+import time
 
 
 @pytest.mark.asyncio
 async def test_homepage():
     """This test assumes that the PetStore API service is running"""
+
+    # Give some time to the app to start up
+    time.sleep(4)
 
     async with aiohttp.ClientSession() as session:
         async with session.get("http://localhost:8000/") as response:
