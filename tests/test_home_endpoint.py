d3838a3ebae0d2e075ca83dbca3b72e5af348188
Kostas <Ziovas>
2021-12-25 13:35:19 +0200

    Add bcrypt in requirements

--- tests/test_home_endpoint.py
+++ tests/test_home_endpoint.py
@@ -8,7 +8,7 @@ async def test_homepage():
     """This test assumes that the PetStore API service is running"""
 
     # Give some time to the app to start up
-    time.sleep(4)
+    time.sleep(2)
 
     async with aiohttp.ClientSession() as session:
         async with session.get("http://localhost:8000/") as response:
